b9bee337547bb3bec7bff08245234c517a48acd5
Eduardo Sánchez <eduardosr1599@gmail.com>
2023-01-14 19:37:43 -0400

    Update App.js file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,38 @@
-function App() {
-    return <div className="App">test</div>;
+import React from 'react';
+import General from './components/General';
+import Education from './components/Education';
+import Experience from './components/Experience';
+
+class EditButton extends React.Component {
+    render() {
+        let button = <button>Edit</button>;
+        if (!this.props.isEditing) {
+            button = null;
+        }
+        return <div>{button}</div>;
+    }
+}
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isEditing: false,
+        };
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <General />
+                <Education />
+                <Experience />
+                <div>
+                    <EditButton isEditing={this.state.isEditing} />
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
